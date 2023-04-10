b4f415f8038cf2f5fe734d8b5e39519b3b85fa9b
SashaLifashkin <alifashkin@outlook.com>
2023-04-11 02:07:53 +0300

    finally MVP with diff situations

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
 import { WordsList } from './components/WordsList';
 
 const initialColors = ['red', 'orange', 'yellow', 'green', 'blue', 'pink', 'white', 'brown', 'black'];
 
-function App() {
+// const debounce = (func: any, delay: number) => {
+const debounce = (func: React.Dispatch<React.SetStateAction<string>>, delay: number) => {
+    // let timerId: NodeJS.Timeout;
+    let timerId: number;
+
+    return (...args: string[]) => {
+        clearTimeout(timerId);
+        timerId = setTimeout(func, delay, ...args);
+    };
+};
+
+function App({ colors = initialColors }) {
+// function App({ initialColors }: { initialColors: string[] }) {
     // const [count, setCount] = useState(0);
     const [query, setQuery] = useState('');
-    // const [appliedQuery, setAppliedQuery] = useState('');
+    const [appliedQuery, setAppliedQuery] = useState('');
 
     // const getVisibleColor = () => {
     //     return initialColors.filter(color => color.startsWith(query));
     // };
 
-    const visibleColors = useMemo(() => {
-        console.log('Filtering: ', query)
+    const applyQuery = useCallback(
+        debounce(setAppliedQuery, 1000),
+        []
+    );
 
-        return initialColors.filter(color => color.startsWith(query));
-    }, [initialColors, query]);
+    const visibleColors = useMemo(() => {
+        console.log('Filtering: ', appliedQuery)
+
+        return colors.filter(color => color.startsWith(appliedQuery));
+    }, [colors, appliedQuery]);
 
     return (
         <div className="App">
@@ -28,8 +45,17 @@ function App() {
             <input
                 type="text"
                 value={query}
-                onChange={event => setQuery(event.target.value)}
+                onChange={event => {
+                    setQuery(event.target.value);
+                    applyQuery(event.target.value)
+                }}
             />
+
+            {/* <input
+                type="text"
+                value={query}
+                onChange={event => setQuery(event.target.value)}
+            /> */}
 
             {/* <button
                 type="button"
