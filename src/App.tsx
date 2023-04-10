ae6046d3206737c6c230210a49ad2f4cc202786b
SashaLifashkin <alifashkin@outlook.com>
2023-04-11 00:41:59 +0300

    first MVP without debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import './App.css';
 import { WordsList } from './components/WordsList';
 
 const initialColors = ['red', 'orange', 'yellow', 'green', 'blue', 'pink', 'white', 'brown', 'black'];
 
 function App() {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
     const [query, setQuery] = useState('');
     // const [appliedQuery, setAppliedQuery] = useState('');
 
-    const gerVisibleColor = () => {
-        return initialColors.filter(color => color.startsWith(query));
-    };
+    // const getVisibleColor = () => {
+    //     return initialColors.filter(color => color.startsWith(query));
+    // };
 
-    const visibleColors = gerVisibleColor();
+    const visibleColors = useMemo(() => {
+        console.log('Filtering: ', query)
+
+        return initialColors.filter(color => color.startsWith(query));
+    }, [initialColors, query]);
 
     return (
         <div className="App">
@@ -27,14 +31,14 @@ function App() {
                 onChange={event => setQuery(event.target.value)}
             />
 
-            <button
+            {/* <button
                 type="button"
                 onClick={() => setCount(count + 1)}
             >
                 {count}++
-            </button>
+            </button> */}
 
-            <WordsList words={visibleColors}/>
+            <WordsList words={visibleColors} />
         </div>
     );
 }
